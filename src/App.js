31510bd4bb2e52ffa5e810a9c999fca7fc962a8d
CV SAMEEHANA <cvsameehana@gmail.com>
2023-12-22 22:54:05 +0530

    made about us page and added dark mode feature using state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
     <Navbar title="TextUtils" aboutText="About Us"/>
-    <div className="container my-3">
+    {/* <div className="container my-3">
       <TextForm heading="Enter the text to analyze below"/>
-    </div>
+    </div> */}
+    <About />
     </>
   );
 }
